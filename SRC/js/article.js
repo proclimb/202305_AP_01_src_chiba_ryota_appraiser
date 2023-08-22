b166d25d3a80faa46c3801e0b863ac018718a2cb
chiba <202305ap01_chiba@x-ide.sakura.ne.jp>
2023-08-22 12:41:49 +0900

    Redmineのチケットナンバー　　#14889
    不具合状況の説明
    1.物件名が入力されているとエラーが出て、未入力だとエラーが出ない逆転して動作している不具合
    2.文字数入力のエラーチェックが行われていない不具合
    3.「OK」「キャンセル」のポップアップが表示されない不具合
    4.エラー表示後に登録確認のポップアップが表示される不具合
    
    修正内容の説明
    1.物件名が入力されていないときにエラーが出るよう修正
    2.文字数チェックが行われるよう修正
    3.「OK」「キャンセル」のポップアップが表示されるよう修正
    4.エラー表示後に登録確認のポップアップが表示されないよう修正

--- SRC/js/article.js
+++ SRC/js/article.js
@@ -3,14 +3,23 @@
 //
 function fnArticleEditCheck() {
 	tmp = form.article.value;
-	if (tmp) {
+	if (tmp == '') { //入力チェックが正常に行えるよう修正
 		alert('物件名を入力してください');
 		return;
 	}
-	if (isLength(100, "物件名", form.article)) { return; }
+	if (isLength(100, "物件名", form.article)) { return; } //入力チェックが正常に行えるよう追加
+	if (isLength(100, "部屋番号", form.room)) { return; } //同上
+	if (isLength(200, "鍵場所", form.keyPlace)) { return; } //同上
+	if (isLength(100, "住所", form.address)) { return; } //同上
+	if (isLength(200, "備考", form.articleNote)) { return; } //同上
+	if (isLength(100, "キーBox番号", form.keyBox)) { return; } //同上
+	if (isLength(100, "3Dパース", form.drawing)) { return; } //同上
+	if (isLength(100, "営業担当者", form.sellCharge)) { return; } //同上
 
-	form.act.value = 'articleEditComplete';
-	form.submit();
+	if (confirm('この内容で登録します。よろしいですか？')) { //ポップアップが表示されるよう修正
+		form.act.value = 'articleEditComplete';
+		form.submit();
+	}
 }
 
 
--- SRC/js/common.js
+++ SRC/js/common.js
@@ -33,6 +33,7 @@ function isLength(length, msg, obj) {
 	rtn = false;
 	if (obj.value.length > length) {
 		alert(msg + "は" + length + "文字以内で入力して下さい");
+		rtn = true; //リターンの値が33行目で「rtn = false」で設定されていたため、別場所で使用しているif分が全てfalseにあるので修正
 	}
 	return rtn;
 }
