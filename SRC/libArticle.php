a6ead0bb5bbfc619f3dffeb25c57a4aa8ce388c5
chiba <202305ap01_chiba@x-ide.sakura.ne.jp>
2023-08-23 16:11:41 +0900

    Redmineのチケットナンバー　　#15068
    不具合状況の説明
    1.検索欄にクロスサイトスクリプティングに関するワードがある場合
    1-1.検索すると検索欄に異常が発生する不具合
    1-2.新規登録・更新画面に遷移すると画面表示に異常が発生する不具合
    
    2.登録されているデータにクロスサイトスクリプティングに関するワードがある場合
    2-1.検索結果の画面表示に異常が発生する不具合
    2-2.更新しようとするデータにクロスサイトスクリプティングに関するワードがある時に入力欄に異常が発生する不具合
    
    修正内容の説明
    文字列として表示させるよう修正

--- SRC/libArticle.php
+++ SRC/libArticle.php
@@ -6,7 +6,7 @@ function subArticle()
 {
 	$conn = fnDbConnect();
 
-	$sDel         = htmlspecialchars($_REQUEST['sDel']);
+	$sDel         = htmlspecialchars($_REQUEST['sDel']); //検索項目にクロスサイトスクリプティングができるワードが入っている状態で検索すると画面表示に異常が発生してしまうため修正
 	$sArticle     = htmlspecialchars($_REQUEST['sArticle']);
 	$sRoom        = htmlspecialchars($_REQUEST['sRoom']);
 	$sKeyPlace    = htmlspecialchars($_REQUEST['sKeyPlace']);
@@ -112,14 +112,14 @@ function subArticle()
 			$res = mysqli_query($conn, $sql);
 			$i = 0;
 			while ($row = mysqli_fetch_array($res)) {
-				$articleNo   = $row["ARTICLENO"];
-				$article     = $row["ARTICLE"];
-				$room        = $row["ROOM"];
-				$keyPlace    = $row["KEYPLACE"];
-				$articleNote =  $row["ARTICLENOTE"];
-				$keyBox      = $row["KEYBOX"];
-				$drawing     = $row["DRAWING"];
-				$sellCharge  = $row["SELLCHARGE"];
+				$articleNo   = htmlspecialchars($row["ARTICLENO"]); //検索結果にクロスサイトスクリプティングができるワードが含まれていると画面表示に異常が発生してしまうので修正
+				$article     = htmlspecialchars($row["ARTICLE"]);
+				$room        = htmlspecialchars($row["ROOM"]);
+				$keyPlace    = htmlspecialchars($row["KEYPLACE"]);
+				$articleNote = htmlspecialchars($row["ARTICLENOTE"]);
+				$keyBox      = htmlspecialchars($row["KEYBOX"]);
+				$drawing     = htmlspecialchars($row["DRAWING"]);
+				$sellCharge  = htmlspecialchars($row["SELLCHARGE"]);
 			?>
 				<tr>
 					<td class="list_td<?php print $i ?>"><a href="javascript:form.act.value='articleEdit';form.articleNo.value='<?php print $articleNo ?>';form.submit();"><?php print $article ?></a></td> <!--変数のスペルが間違っていた(print $rrticleNo)ため物件更新に遷移せず物件登録に遷移していたため修正-->
@@ -150,15 +150,15 @@ function subArticleEdit()
 {
 	$conn = fnDbConnect();
 
-	$sDel         = $_REQUEST['sDel'];
-	$sArticle     = $_REQUEST['sArticle'];
-	$sRoom        = $_REQUEST['sRoom'];
-	$sKeyPlace    = $_REQUEST['sKeyPlace'];
-	$sArticleNote = $_REQUEST['sArticleNote'];
-	$sKeyBox      = $_REQUEST['sKeyBox'];
-	$sDueDTFrom   = $_REQUEST['sDueDTFrom'];
-	$sDueDTTo     = $_REQUEST['sDueDTTo'];
-	$sSellCharge  = $_REQUEST['sSellCharge'];
+	$sDel         = htmlspecialchars($_REQUEST['sDel']); //検索欄にクロスサイトスクリプティングに関するワードが入った状態で新規登録・更新画面に遷移すると画面表示に異常が発生するため修正
+	$sArticle     = htmlspecialchars($_REQUEST['sArticle']);
+	$sRoom        = htmlspecialchars($_REQUEST['sRoom']);
+	$sKeyPlace    = htmlspecialchars($_REQUEST['sKeyPlace']);
+	$sArticleNote = htmlspecialchars($_REQUEST['sArticleNote']);
+	$sKeyBox      = htmlspecialchars($_REQUEST['sKeyBox']);
+	$sDueDTFrom   = htmlspecialchars($_REQUEST['sDueDTFrom']);
+	$sDueDTTo     = htmlspecialchars($_REQUEST['sDueDTTo']);
+	$sSellCharge  = htmlspecialchars($_REQUEST['sSellCharge']);
 
 	$orderBy = $_REQUEST['orderBy'];
 	$orderTo = $_REQUEST['orderTo'];
@@ -171,15 +171,15 @@ function subArticleEdit()
 		$res = mysqli_query($conn, $sql);
 		$row = mysqli_fetch_array($res);
 
-		$article     =  $row["ARTICLE"];
-		$room        =  $row["ROOM"];
-		$keyPlace    =  $row["KEYPLACE"];
-		$address     =  $row["ADDRESS"];
-		$articleNote =  $row["ARTICLENOTE"];
-		$keyBox      =  $row["KEYBOX"];
-		$drawing     =  $row["DRAWING"];
-		$sellCharge  =  $row["SELLCHARGE"];
-		$del         =  $row["DEL"];
+		$article     = htmlspecialchars($row["ARTICLE"]); //登録してある情報にクロスサイトスクリプティングに関するワードが含まれていると更新時の画面表示に異常が発生するため修正
+		$room        = htmlspecialchars($row["ROOM"]);
+		$keyPlace    = htmlspecialchars($row["KEYPLACE"]);
+		$address     = htmlspecialchars($row["ADDRESS"]);
+		$articleNote = htmlspecialchars($row["ARTICLENOTE"]);
+		$keyBox      = htmlspecialchars($row["KEYBOX"]);
+		$drawing     = htmlspecialchars($row["DRAWING"]);
+		$sellCharge  = htmlspecialchars($row["SELLCHARGE"]);
+		$del         = htmlspecialchars($row["DEL"]);
 
 		$purpose  = '更新';
 		$btnImage = 'btn_load.png';
