7fcdb50710969d68ee87c389cef10c01716c7e50
chiba <202305ap01_chiba@x-ide.sakura.ne.jp>
2023-08-30 11:00:46 +0900

    Redmineのチケットナンバー　　#15398
    不具合状況の説明
    新規登録時、DBに登録されず白紙のページに遷移する
    
    修正内容の説明
    DBに登録され業者管理一覧に遷移するよう修正

--- SRC/js/trade.js
+++ SRC/js/trade.js
@@ -73,7 +73,7 @@ function fnTradeEditCheck() {
 	}
 
 	if (confirm('この内容で登録します。よろしいですか？')) {
-		form.act.value = 'tradeEditCompleta';
+		form.act.value = 'tradeEditComplete';//スペルミスにより登録処理を行えずページ遷移もできなかったため修正
 		form.submit();
 	}
 }
